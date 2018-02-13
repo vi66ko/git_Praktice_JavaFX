1f4556d6878bdbd97e9de0a4570e8ba87cbd0467
Vi66ko <vi66ko@mail.bg>
2018-02-13 22:09:52 +0000

    First commitment of Main Class

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,8 +12,9 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 300, 400));
         primaryStage.show();
+        // One line more
     }
 
 
